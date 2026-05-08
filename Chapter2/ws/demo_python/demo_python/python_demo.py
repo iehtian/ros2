6b6bcb2bd8ce13a4eb5fe3b2cdbe65fc8ed5617d
iehtian <hao.tian@3d-hsi.com>
2026-05-08 10:36:14 +0800

    update

---
+++ Chapter2/ws/demo_python/demo_python/python_demo.py
@@ -0,0 +1,11 @@
+import rclpy
+from rclpy.node import Node
+
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = Node("first_node")
+    node.get_logger().info("节点已启动: first_node")
+    rclpy.spin(node)
+    node.destroy_node()
+    rclpy.shutdown()
---
+++ Chapter2/ws/demo_python/setup.py
@@ -0,0 +1,29 @@
+from setuptools import find_packages, setup
+
+package_name = "demo_python"
+
+setup(
+    name=package_name,
+    version="0.0.0",
+    packages=find_packages(exclude=["test"]),
+    data_files=[
+        ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
+        ("share/" + package_name, ["package.xml"]),
+    ],
+    install_requires=["setuptools"],
+    zip_safe=True,
+    maintainer="iehtian",
+    maintainer_email="hao.tian@3d-hsi.com",
+    description="TODO: Package description",
+    license="TODO: License declaration",
+    extras_require={
+        "test": [
+            "pytest",
+        ],
+    },
+    entry_points={
+        "console_scripts": [
+            "first_node = demo_python.python_demo:main",
+        ],
+    },
+)
